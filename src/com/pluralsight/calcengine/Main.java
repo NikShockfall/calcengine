b3a228b69f7fdd83d2f406a1d253ee41bdeb1220
NikShockfall <83179923+NikShockfall@users.noreply.github.com>
2021-05-01 02:01:12 +0800

    First implementation of MathEquation
    Implementation of MathEquation, utilizing displayResult instead of a separate display code

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -6,23 +6,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
         if (args.length == 0){
-            for (int i = 0; i < opCodes.length; i++) {
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
+            performCalculations();
 
-        for (double currentResult : results)
-            System.out.println(currentResult);
         } else if(args.length == 1 && args[0].equals("interactive"))
             executeInteractively();
         else if(args.length == 3)
             performOperation(args);
-            else System.out.println("Please provide an operation code and 2 numeric values.");
+        else{
+            System.out.println("Please provide an operation code and 2 numeric values.");
+            executeInteractively();
+        }
+    }
+
+    static void performCalculations(){
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
+
+        for(MathEquation equation : equations){
+            equation.execute();
+            displayResult(equation.opCode, equation.leftVal, equation.rightVal, equation.result);
+        }
+    }
+
+    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+        return equation;
     }
 
     static void executeInteractively() {
@@ -80,25 +95,19 @@ public class Main {
 
 
     static double execute(char opCode, double leftVal, double rightVal){
-        double result;
         switch (opCode) {
             case 'a':
-                result =leftVal + rightVal;
-                break;
+                return leftVal + rightVal;
             case 's':
-                result =leftVal - rightVal;
-                break;
+                return leftVal - rightVal;
             case 'm':
-                result =leftVal * rightVal;
-                break;
+                return leftVal * rightVal;
             case 'd':
-                result =rightVal != 0 ? leftVal / rightVal : 0.0d;
-                break;
+                return rightVal != 0 ? leftVal / rightVal : 0.0d;
             default:
                 System.out.println("Invalid operation code: " + opCode);
-                result =0.0d;
+                return 0.0d;
         }
-        return result;
     }
 
     static char opCodeFromString(String operationName){
